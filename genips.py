106f231aa18ca6477cb0f86c56a21d1ffe88aea2
b0b013 <97jeepwranglerforsale@gmail.com>
2018-01-18 17:20:10 -0800

    genips

---
+++ genips.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys
+# genips.py
+# usage: genips.py network ip_start ip_end filename
+# reads generates ip list
+# really basic. think class C network for now.
+#***************************************
+network =  sys.argv[1]
+ip_start = int(sys.argv[2])
+ip_end = int(sys.argv[3]) + 1
+#file =  open(sys.argv[4],"w")
+for x in range(ip_start, ip_end):
+	print network + '.' + str(x)
+#f.close()
