22b7d5ff08027bc97ed35411bf0a5f9a18c704b7
b0b013 <97jeepwranglerforsale@gmail.com>
2018-01-18 17:02:45 -0800

    readfile

--- readfile.py
+++ readfile.py
@@ -4,9 +4,9 @@ import sys
 # listips.py
 # usage: listips.py filename
 # reads file line by line and prints it
-***************************************
+#***************************************
 
 f = open(sys.argv[1],"r")
 for line in f:
-	print line
+	print line,
 f.close()
