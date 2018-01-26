15e78b8fd3b40ce57f196091c2a9a84757dc8dcc
b0b013 <97jeepwranglerforsale@gmail.com>
2018-01-25 20:22:41 -0800

    stub file

--- enumscript.py
+++ enumscript.py
@@ -2,6 +2,11 @@
 
 import sys
 # enumscript.py
-# usage: enumscript.py file
+# usage: enumscript.py file [> enum.txt]
 # reads ip list and calls enum4linux for each ip
+# Next version: write all output to one file
 #***************************************
+f = open(sys.argv[1],"r")
+for line in f:
+	# call enum4linux
+f.close()
