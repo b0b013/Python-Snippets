3401cb81cccfc5603c1b8104c25cae69f73000c1
b0b013 <97jeepwranglerforsale@gmail.com>
2018-01-25 20:25:59 -0800

    first attempt at subprocess call.

--- enumscript.py
+++ enumscript.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import subprocess
 # enumscript.py
 # usage: enumscript.py file [> enum.txt]
 # reads ip list and calls enum4linux for each ip
@@ -9,4 +10,5 @@ import sys
 f = open(sys.argv[1],"r")
 for line in f:
 	# call enum4linux
+    subprocess.call("enum4linux", line)
 f.close()
